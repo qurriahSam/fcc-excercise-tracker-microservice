4ca42040615eec497fa3ee3f449cf88b8c314faf
qurriahSam <mugirethe1@gmail.com>
2023-05-10 19:35:27 +0300

    feat: create route to add user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const connectDB = require("./db/database");
+const User = require("./schema/user");
 connectDB();
 
 app.use(cors());
@@ -14,7 +15,22 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", (req, res) => {});
+app.post("/api/users", async (req, res) => {
+  const person = new User({
+    username: req.body.username,
+  });
+  try {
+    const result = await person.save();
+    res.json({
+      username: result.username,
+      _id: result._id.toString(),
+    });
+    console.log(result);
+  } catch (error) {
+    res.json({ error: error.message });
+    console.error(error);
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);

c6486324b3ef3dda6a60a50aadcda24f73c7fa7e
qurriahSam <mugirethe1@gmail.com>
2023-05-11 17:57:34 +0300

    feat: enable fetching of all users

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const connectDB = require("./db/database");
 const User = require("./schema/user");
 connectDB();
 
+morgan("tiny");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -21,14 +23,26 @@ app.post("/api/users", async (req, res) => {
   });
   try {
     const result = await person.save();
-    res.json({
+    res.status(200).json({
       username: result.username,
       _id: result._id.toString(),
     });
     console.log(result);
   } catch (error) {
-    res.json({ error: error.message });
-    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  let filteredUsers = [];
+  try {
+    const users = await User.find({});
+    users.forEach((user) => {
+      filteredUsers.push({ username: user.username, _id: user._id.toString() });
+    });
+    return res.status(200).json(filteredUsers);
+  } catch (error) {
+    return res.status(500).json({});
   }
 });
 

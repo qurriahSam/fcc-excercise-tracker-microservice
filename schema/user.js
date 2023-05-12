4961d366404e27d316d70422692564a333f15f4f
qurriahSam <mugirethe1@gmail.com>
2023-05-12 22:20:04 +0300

    feat: add count and link to exercises

--- schema/user.js
+++ schema/user.js
@@ -2,6 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const User = new Schema({
   username: String,
+  count: Number,
+  exercises: [{ type: Schema.Types.ObjectId, ref: "exercise" }],
 });
 
 const UserModel = model("user", User);
